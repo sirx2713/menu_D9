450b81bf0e1dc745f7811c5a7a0fc150638794f9
Sirx <bradx2713@gmail.com>
2024-02-29 10:57:31 +0200

    Initial Commit

---
+++ menu_D9/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  menu_D9
+//
+//  Created by Tafadzwa Alexander Razaro on 2024/02/29.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ menu_D9/menu_D9App.swift
@@ -0,0 +1,17 @@
+//
+//  menu_D9App.swift
+//  menu_D9
+//
+//  Created by Tafadzwa Alexander Razaro on 2024/02/29.
+//
+
+import SwiftUI
+
+@main
+struct menu_D9App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
