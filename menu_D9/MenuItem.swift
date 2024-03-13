4858a372f8098d85abd11670c45ec7385e2a6d1b
sirx2713 <bradx2713@gmail.com>
2024-03-13 03:24:30 +0300

    09 Attempts of Making the Menu Application.

---
+++ menu_D9/MenuItem.swift
@@ -0,0 +1,17 @@
+//
+//  MenuItem.swift
+//  menu_D9
+//
+//  Created by Tafadzwa Alexander Razaro on 2024/02/29.
+//
+
+import Foundation
+
+struct MenuItem: Identifiable {
+    
+    var id: UUID = UUID()
+    var name:String
+    var price:String
+    var imageName:String
+    
+}
--- menu_D9/menu_D9App.swift
+++ menu_D9/menu_D9App.swift
@@ -11,7 +11,7 @@ import SwiftUI
 struct menu_D9App: App {
     var body: some Scene {
         WindowGroup {
-            ContentView()
+            MenuView()
         }
     }
 }
